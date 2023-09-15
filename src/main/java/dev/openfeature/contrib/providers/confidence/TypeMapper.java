77e880971285c1473add10fd2bb574b09830ef2a
Nicklas Lundin <nicklasl@spotify.com>
2023-09-15 11:27:13 +0200

    initial move from internal repo

---
+++ src/main/java/dev/openfeature/contrib/providers/confidence/TypeMapper.java
@@ -0,0 +1,125 @@
+package dev.openfeature.contrib.providers.confidence;
+
+import com.google.protobuf.Struct;
+import com.google.protobuf.util.Values;
+import com.spotify.confidence.flags.types.v1.FlagSchema;
+import com.spotify.confidence.flags.types.v1.FlagSchema.SchemaTypeCase;
+import com.spotify.confidence.flags.types.v1.FlagSchema.StructFlagSchema;
+import dev.openfeature.sdk.MutableStructure;
+import dev.openfeature.sdk.Structure;
+import dev.openfeature.sdk.Value;
+import dev.openfeature.sdk.exceptions.ParseError;
+import dev.openfeature.sdk.exceptions.ValueNotConvertableError;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+// For now, only package visibility to keep control on this part of the code
+class TypeMapper {
+
+  private static Value from(com.google.protobuf.Value value, FlagSchema schema) {
+
+    if (schema.getSchemaTypeCase() == SchemaTypeCase.SCHEMATYPE_NOT_SET) {
+      throw new ParseError("schemaType not set in FlagSchema");
+    }
+
+    final String mismatchPrefix = "Mismatch between schema and value:";
+    switch (value.getKindCase()) {
+      case NULL_VALUE:
+        try {
+          return new Value((Object) null);
+        } catch (InstantiationException e) {
+          throw new RuntimeException(e);
+        }
+      case NUMBER_VALUE:
+        switch (schema.getSchemaTypeCase()) {
+          case INT_SCHEMA:
+            final long intVal = (long) value.getNumberValue();
+            if (intVal != value.getNumberValue()) {
+              throw new ParseError(
+                  String.format("%s value should be an int64, but it is a double", mismatchPrefix));
+            }
+            return new Value(value.getNumberValue());
+          case DOUBLE_SCHEMA:
+            return new Value(value.getNumberValue());
+          default:
+            throw new ParseError("Number field must have schema type int or double");
+        }
+      case STRING_VALUE:
+        if (schema.getSchemaTypeCase() != SchemaTypeCase.STRING_SCHEMA) {
+          throw new ParseError(
+              String.format(
+                  "%s value is a String, but it should be something else", mismatchPrefix));
+        }
+        return new Value(value.getStringValue());
+      case BOOL_VALUE:
+        if (schema.getSchemaTypeCase() != SchemaTypeCase.BOOL_SCHEMA) {
+          throw new ParseError(
+              String.format("%s value is a bool, but should be something else", mismatchPrefix));
+        }
+        return new Value(value.getBoolValue());
+      case STRUCT_VALUE:
+        if (schema.getSchemaTypeCase() != SchemaTypeCase.STRUCT_SCHEMA) {
+          throw new ParseError(
+              String.format("%s value is a struct, but should be something else", mismatchPrefix));
+        }
+
+        return from(value.getStructValue(), schema.getStructSchema());
+      case LIST_VALUE:
+        if (schema.getSchemaTypeCase() != SchemaTypeCase.LIST_SCHEMA) {
+          throw new ParseError(
+              String.format("%s value is a list, but should be something else", mismatchPrefix));
+        }
+
+        final List<Value> mappedList =
+            value.getListValue().getValuesList().stream()
+                .map(val -> from(val, schema.getListSchema().getElementSchema()))
+                .collect(Collectors.toList());
+        return new Value(mappedList);
+      case KIND_NOT_SET:
+        throw new ParseError("kind not set in com.google.protobuf.Value");
+      default:
+        throw new ParseError("Unknown value type");
+    }
+  }
+
+  public static Value from(Struct struct, StructFlagSchema schema) {
+    final Map<String, Value> map =
+        struct.getFieldsMap().entrySet().stream()
+            .collect(
+                Collectors.toMap(
+                    Map.Entry::getKey,
+                    entry -> {
+                      if (schema.getSchemaMap().containsKey(entry.getKey())) {
+                        return from(entry.getValue(), schema.getSchemaMap().get(entry.getKey()));
+                      } else {
+                        throw new ParseError(
+                            String.format("Lacking schema for field '%s'", entry.getKey()));
+                      }
+                    }));
+
+    return new Value(new MutableStructure(map));
+  }
+
+  public static com.google.protobuf.Value from(Value val) {
+    if (val.isBoolean()) {
+      return Values.of(val.asBoolean());
+    } else if (val.isNull()) {
+      return Values.ofNull();
+    } else if (val.isInstant()) {
+      throw new ValueNotConvertableError("Converting Instant Value is currently not supported");
+    } else if (val.isString()) {
+      return Values.of(val.asString());
+    } else if (val.isList()) {
+      final List<Value> values = val.asList();
+      return Values.of(values.stream().map(TypeMapper::from).collect(Collectors.toList()));
+    } else if (val.isStructure()) {
+      final Structure structure = val.asStructure();
+      final Map<String, com.google.protobuf.Value> protoMap =
+          structure.asMap().keySet().stream()
+              .collect(Collectors.toMap(key -> key, key -> from(structure.getValue(key))));
+      return Values.of(Struct.newBuilder().putAllFields(protoMap).build());
+    }
+    throw new ValueNotConvertableError("Unknown Value type");
+  }
+}
---
+++ src/main/java/dev/openfeature/contrib/providers/confidence/package-info.java
@@ -0,0 +1,4 @@
+@ParametersAreNonnullByDefault
+package dev.openfeature.contrib.providers.confidence;
+
+import javax.annotation.ParametersAreNonnullByDefault;
