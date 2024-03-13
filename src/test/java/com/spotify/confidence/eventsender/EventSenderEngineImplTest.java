58026cf94d3ccb9a75af253c5bd2b7dd09b4fd17
Nicklas Lundin <nicklasl@spotify.com>
2024-03-13 08:58:19 +0100

    throw exceptions from tests

--- src/test/java/com/spotify/confidence/eventsender/EventSenderEngineImplTest.java
+++ src/test/java/com/spotify/confidence/eventsender/EventSenderEngineImplTest.java
@@ -8,65 +8,57 @@ import java.io.IOException;
 import java.util.List;
 import java.util.Set;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.junit.jupiter.MockitoExtension;
 
-@ExtendWith(MockitoExtension.class)
 public class EventSenderEngineImplTest {
 
   @Test
-  public void testEngineUploads() {
+  public void testEngineUploads() throws InterruptedException, IOException {
     IntermittentErrorUploader alwaysSucceedUploader = new IntermittentErrorUploader(List.of());
     int batchSize = 6;
     int numEvents = 14;
-    try (EventSenderEngine engine =
-        new EventSenderEngineImpl(getFlushPolicies(10000, batchSize), alwaysSucceedUploader)) {
-      int size = 0;
-      while (size++ < numEvents) {
-        engine.send(
-            "eventDefinitions/navigate",
-            ConfidenceValue.of(ImmutableMap.of("key", ConfidenceValue.of("size"))));
-      }
-      Thread.sleep(1000); // Wait for "close" and the correct uploading of events
-
-      engine.close(); // Should trigger the upload of an additional incomplete batch
-      int additionalBatch = (numEvents % batchSize) > 0 ? 1 : 0;
-
-      assertThat(alwaysSucceedUploader.uploadCalls.size())
-          .isEqualTo((numEvents / batchSize + additionalBatch));
-    } catch (IOException | InterruptedException e) {
-      throw new RuntimeException(e);
+    EventSenderEngine engine =
+        new EventSenderEngineImpl(getFlushPolicies(10000, batchSize), alwaysSucceedUploader);
+    int size = 0;
+    while (size++ < numEvents) {
+      engine.send(
+          "eventDefinitions/navigate",
+          ConfidenceValue.of(ImmutableMap.of("key", ConfidenceValue.of("size"))));
     }
+    Thread.sleep(1000); // Wait for "close" and the correct uploading of events
+
+    engine.close(); // Should trigger the upload of an additional incomplete batch
+    int additionalBatch = (numEvents % batchSize) > 0 ? 1 : 0;
+
+    assertThat(alwaysSucceedUploader.uploadCalls.size())
+        .isEqualTo((numEvents / batchSize + additionalBatch));
   }
 
   @Test
-  public void testEngineUploadsWhenIntermittentErrorWillRetry() {
+  public void testEngineUploadsWhenIntermittentErrorWillRetry()
+      throws InterruptedException, IOException {
     int batchSize = 3;
     int numEvents = 14;
     // This will fail at the 2nd and 5th upload
     List<Integer> failAtUploadWithIndex = List.of(2, 5);
     IntermittentErrorUploader fakeUploader = new IntermittentErrorUploader(failAtUploadWithIndex);
-    try (EventSenderEngine engine =
-        new EventSenderEngineImpl(getFlushPolicies(10000, batchSize), fakeUploader)) {
-      int size = 0;
-      while (size++ < numEvents) {
-        engine.send(
-            "eventDefinitions/navigate",
-            ConfidenceValue.of(ImmutableMap.of("key", ConfidenceValue.of("size=" + size))));
-      }
-      Thread.sleep(1000); // Wait for "close" and the correct uploading of events
-
-      engine.close(); // Should trigger the upload of an additional incomplete batch
-      int additionalBatch = (numEvents % batchSize) > 0 ? 1 : 0;
-
-      // Verify we had the correct number of calls to the uploader (including retries)
-      assertThat(fakeUploader.uploadCalls.size())
-          .isEqualTo((numEvents / batchSize + additionalBatch) + failAtUploadWithIndex.size());
-      // Verify we had the correct number of unique calls to the uploader
-      assertThat(Set.copyOf(fakeUploader.uploadCalls).size())
-          .isEqualTo((numEvents / batchSize + additionalBatch));
-    } catch (IOException | InterruptedException e) {
-      throw new RuntimeException(e);
+    EventSenderEngine engine =
+        new EventSenderEngineImpl(getFlushPolicies(10000, batchSize), fakeUploader);
+    int size = 0;
+    while (size++ < numEvents) {
+      engine.send(
+          "eventDefinitions/navigate",
+          ConfidenceValue.of(ImmutableMap.of("key", ConfidenceValue.of("size=" + size))));
     }
+    Thread.sleep(1000); // Wait for "close" and the correct uploading of events
+
+    engine.close(); // Should trigger the upload of an additional incomplete batch
+    int additionalBatch = (numEvents % batchSize) > 0 ? 1 : 0;
+
+    // Verify we had the correct number of calls to the uploader (including retries)
+    assertThat(fakeUploader.uploadCalls.size())
+        .isEqualTo((numEvents / batchSize + additionalBatch) + failAtUploadWithIndex.size());
+    // Verify we had the correct number of unique calls to the uploader
+    assertThat(Set.copyOf(fakeUploader.uploadCalls).size())
+        .isEqualTo((numEvents / batchSize + additionalBatch));
   }
 }
