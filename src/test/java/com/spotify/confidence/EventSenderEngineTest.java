7c14ddadad7dee532d5b5a8b227c83d17c20f117
vahid torkaman <vahidt@spotify.com>
2024-04-12 15:57:52 +0200

    add test for batch result to be cancelled on interrupt thread

--- src/test/java/com/spotify/confidence/EventSenderEngineTest.java
+++ src/test/java/com/spotify/confidence/EventSenderEngineTest.java
@@ -202,6 +202,21 @@ public class EventSenderEngineTest {
   }
 
   @Test
+  public void testUnsentEventsAreCancelledOnThreadInterrupted() throws Exception {
+    final CompletableFuture<Boolean> batchResult = new CompletableFuture<>();
+    final EventUploader fakeUploader = events -> batchResult;
+
+    // set up the engine so that it cannot support more than 1 event in memory
+    final EventSenderEngineImpl engine =
+        new EventSenderEngineImpl(10, fakeUploader, clock, Duration.ofMillis(10), 1024);
+    engine.send("fake", ConfidenceValue.Struct.EMPTY, Optional.empty());
+    Thread.sleep(100);
+    Thread.currentThread().interrupt();
+    engine.close();
+    assertThat(batchResult.isCancelled()).isTrue();
+  }
+
+  @Test
   public void testEngineWillRejectEventsIfOverMemoryThreshold() throws IOException {
     final var expectedEvent =
         EventUploader.event("navigate", ConfidenceValue.Struct.EMPTY, Optional.empty())
