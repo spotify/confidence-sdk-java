918241faf977737d97dbbd5d11ec6e84d008d20e
Fabrizio Demaria <fabrizio.f.demaria@gmail.com>
2024-05-21 09:22:48 +0200

    refactor: Remove lombok dependency

--- src/main/java/com/spotify/confidence/FlagEvaluation.java
+++ src/main/java/com/spotify/confidence/FlagEvaluation.java
@@ -1,18 +1,76 @@
 package com.spotify.confidence;
 
 import java.util.Optional;
-import lombok.Data;
-import lombok.Getter;
+import javax.annotation.Nonnull;
 
-@Data
-@Getter
 public class FlagEvaluation<T> {
+
+  private T value;
+  private String variant;
+  private String reason;
+  private Optional<ErrorType> errorType;
+  private Optional<String> errorMessage;
+
+  public FlagEvaluation(
+      @Nonnull T value,
+      @Nonnull String variant,
+      @Nonnull String reason,
+      @Nonnull ErrorType errorType,
+      @Nonnull String errorMessage) {
+    this.value = value;
+    this.variant = variant;
+    this.reason = reason;
+    this.errorType = Optional.of(errorType);
+    this.errorMessage = Optional.of(errorMessage);
+  }
+
+  public FlagEvaluation(@Nonnull T value, @Nonnull String variant, @Nonnull String reason) {
+    this.value = value;
+    this.variant = variant;
+    this.reason = reason;
+    this.errorType = Optional.empty();
+    this.errorMessage = Optional.empty();
+  }
 
   public T getValue() {
     return value;
   }
 
-  private T value;
+  public void setValue(T value) {
+    this.value = value;
+  }
+
+  public String getVariant() {
+    return variant;
+  }
+
+  public void setVariant(String variant) {
+    this.variant = variant;
+  }
+
+  public String getReason() {
+    return reason;
+  }
+
+  public void setReason(String reason) {
+    this.reason = reason;
+  }
+
+  public Optional<ErrorType> getErrorType() {
+    return errorType;
+  }
+
+  public void setErrorType(ErrorType errorType) {
+    this.errorType = Optional.of(errorType);
+  }
+
+  public Optional<String> getErrorMessage() {
+    return errorMessage;
+  }
+
+  public void setErrorMessage(String errorMessage) {
+    this.errorMessage = Optional.of(errorMessage);
+  }
 
   @Override
   public String toString() {
@@ -31,27 +89,5 @@ public class FlagEvaluation<T> {
         + errorMessage.orElse("")
         + '\''
         + '}';
-  }
-
-  private String variant;
-  private String reason;
-  private Optional<ErrorType> errorType;
-  private Optional<String> errorMessage;
-
-  public FlagEvaluation(
-      T value, String variant, String reason, ErrorType errorType, String errorMessage) {
-    this.value = value;
-    this.variant = variant;
-    this.reason = reason;
-    this.errorType = Optional.of(errorType);
-    this.errorMessage = Optional.of(errorMessage);
-  }
-
-  public FlagEvaluation(T value, String variant, String reason) {
-    this.value = value;
-    this.variant = variant;
-    this.reason = reason;
-    this.errorType = Optional.empty();
-    this.errorMessage = Optional.empty();
   }
 }
