808aaadb7b460b906cec73cb663b11e45f90a8ed
Fabrizio Demaria <fabrizio.f.demaria@gmail.com>
2025-10-22 14:58:53 +0200

    refactor: Remove local provider due to migration (#316)

--- openfeature-provider-local/src/main/java/com/spotify/confidence/FlagToApply.java
+++
@@ -1,6 +0,0 @@
-package com.spotify.confidence;
-
-import com.spotify.confidence.shaded.flags.resolver.v1.ResolveTokenV1;
-import java.time.Instant;
-
-record FlagToApply(Instant skewAdjustedAppliedTime, ResolveTokenV1.AssignedFlag assignment) {}
--- openfeature-provider-local/src/main/java/com/spotify/confidence/WasmFlagLogger.java
+++
@@ -1,9 +0,0 @@
-package com.spotify.confidence;
-
-import com.spotify.confidence.shaded.flags.resolver.v1.WriteFlagLogsRequest;
-
-interface WasmFlagLogger {
-  void write(WriteFlagLogsRequest request);
-
-  void shutdown();
-}
